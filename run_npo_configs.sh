793e8305eabd1f47b125318f121dcb581076488a
Dogukan <dbaysal2001@gmail.com>
2026-07-05 15:08:54 +0200

    a

--- run_npo_configs.sh
+++ run_npo_configs.sh
@@ -14,9 +14,9 @@ CONFIGS=(
   "configs/npo/qwen2.5coder-3b-unlearned-npo-lr-1e-5-checkpoint4/config.yaml"
   "configs/npo/qwen2.5coder-3b-unlearned-npo-lr-1e-5-checkpoint8/config.yaml"
   "configs/npo/qwen2.5coder-3b-unlearned-npo-lr-1e-5-checkpoint12/config.yaml"
-  "configs/npo/qwen2.5coder-3b-unlearned-npo-lr-5e-5-checkpoint4/config.yaml"
-  "configs/npo/qwen2.5coder-3b-unlearned-npo-lr-5e-5-checkpoint8/config.yaml"
-  "configs/npo/qwen2.5coder-3b-unlearned-npo-lr-5e-5-checkpoint12/config.yaml"
+  # "configs/npo/qwen2.5coder-3b-unlearned-npo-lr-5e-5-checkpoint4/config.yaml"
+  # "configs/npo/qwen2.5coder-3b-unlearned-npo-lr-5e-5-checkpoint8/config.yaml"
+  # "configs/npo/qwen2.5coder-3b-unlearned-npo-lr-5e-5-checkpoint12/config.yaml"
 )
 
 for config in "${CONFIGS[@]}"; do
